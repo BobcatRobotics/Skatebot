094721a324b0d0b003d43141b4f628c04ee23c2d
Kevin <kevinryan.sw@gmail.com>
2018-10-26 14:52:39 -0400

    added two folders

---
+++ src/main/java/org/usfirst/frc/team177/commands/DriveWithJoysticks.java
@@ -0,0 +1,41 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team177.commands;
+
+import org.usfirst.frc.team177.robot.OI;
+import org.usfirst.frc.team177.robot.RobotMap;
+
+public class DriveWithJoysticks extends DriveCommand {
+	
+	public DriveWithJoysticks() {
+		super();
+	}
+
+	@Override
+	protected void execute() {
+		// Driving
+		//double left = OI.leftStick.getRawAxis(Joystick.AxisType.kY.value);
+		//double right = OI.rightStick.getRawAxis(Joystick.AxisType.kY.value);
+		double left = OI.gamePad.getRawAxis(RobotMap.gamePadElevatorCommandStick);
+		double right = OI.gamePad.getRawAxis(RobotMap.gamePadClimberArmCommandStick); //Move
+		//DriverStation.reportError("left stick value: " + left + " right stick value " + right, false);
+		OI.driveTrain.setLeftPower(left);
+		OI.driveTrain.setRightPower(right);
+		OI.driveTrain.drive();
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
+
+	@Override
+	protected void end() {
+		OI.driveTrain.stop();
+	}
+}
---
+++ src/main/java/org/usfirst/frc/team177/subsystems/ExampleSubsystem.java
@@ -0,0 +1,18 @@
+
+package org.usfirst.frc.team177.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * An example subsystem.  You can replace me with your own Subsystem.
+ */
+public class ExampleSubsystem extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
